f9e7606e44fd43e3823e1590b07ee69d0127fbe4
Jake Meyers <jakejake92@yahoo.com>
2021-02-23 16:17:35 -0700

    project init

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useContext } from "react";
+import "./App.css";
+
+const themes = {
+	light: {
+		foreground: "#000000",
+		background: "#eeeeee",
+	},
+	dark: {
+		foreground: "#ffffff",
+		background: "#222222",
+	},
+};
+
+const ThemeContext = React.createContext(themes.light);
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<div className='App'>
+			<ThemeContext.Provider value={themes.dark}>
+				<h1>hello world</h1>
+				<h2>the world is beautiful</h2>
+			</ThemeContext.Provider>
+		</div>
+	);
 }
 
 export default App;
